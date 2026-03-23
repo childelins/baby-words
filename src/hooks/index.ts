259488cbd4ec76e0e40c563ec1bc117c3affc486
childelins <childelins@gmail.com>
2026-03-23 17:51:41 +0800

    feat(theme): 添加 useTheme 主题切换 Hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,5 @@
 export { useAutoPlay } from './useAutoPlay';
 export { useRipple } from './useRipple';
 export { useMascotTheme } from './useMascotTheme';
+export { useTheme } from './useTheme';
+export type { Theme } from './useTheme';
